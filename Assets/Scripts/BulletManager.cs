31e15b46681e8385ab3a6affe31084960c017ee4
André <imandreleandro@gmail.com>
2025-04-13 18:13:20 +0100

    Added Enemy with Shooting when player is in front of it, enemy pathing auto pathing, player 4 directions and shooting where it's facing depending on it's last input

---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+
+    private float _bulletSpeed = 250f;
+    private Rigidbody2D _rb;
+    private Vector2 _direction = Vector2.right;
+    public Vector2 SetDirection(Vector2 direction) => _direction = direction;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = _direction * _bulletSpeed;
+    }
+}
