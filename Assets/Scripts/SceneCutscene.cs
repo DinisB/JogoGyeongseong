25ff604ad2f140f2386336037572758dbe4e0d9a
André <imandreleandro@gmail.com>
2025-05-06 08:09:26 +0100

    Added Script for cutscenes and new scene as example with it

---
+++ Assets/Scripts/SceneCutscene.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneCutscene : MonoBehaviour
+{
+
+    [SerializeField] private Sprite[] sprites;
+    [SerializeField] private float timeEachFrame = 0.2f;
+    [SerializeField] private Image canvasImageObject;
+    [SerializeField] private bool loadNextScene = false;
+    [SerializeField] private string nextSceneToLoad;
+
+    private void Start()
+    {
+        StartCoroutine(Cutscene());
+    }
+
+    private IEnumerator Cutscene()
+    {
+        int count = 0;
+        while (true)
+        {
+            canvasImageObject.sprite = sprites[count];
+            count++;
+            if (count >= sprites.Length) break;
+            yield return new WaitForSeconds(timeEachFrame);
+        }
+        yield return null;
+        if (!SceneManager.GetSceneByName(nextSceneToLoad).IsValid() || !loadNextScene) yield break;
+        SceneManager.LoadSceneAsync(nextSceneToLoad);
+    }
+    
+}
